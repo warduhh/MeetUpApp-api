dd9dea8c16b67e83314fc7b7b82ad5136162e973
Warda Nur <warduhh@gmail.com>
2023-07-30 18:10:22 +0000

    db file to connect to server

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+// Load environment variables
+require('dotenv').config();
+
+
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+};
+
+const db = new Pool(dbParams);
+
+
+
+module.exports = db;
+
