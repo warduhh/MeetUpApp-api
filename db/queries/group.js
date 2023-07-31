5a29e0351f64e7e53a76495b7301912c6398434e
Warda Nur <warduhh@gmail.com>
2023-07-31 00:28:58 +0000

    group queries created

---
+++ db/queries/group.js
@@ -0,0 +1,94 @@
+const pool = require('../index');
+
+//Create a new group
+const createGroup = function (groupName, groupDescription, organizerId) {
+  return pool
+    .query(
+      "INSERT INTO groups (groupName, groupDescription, organizerId) VALUES ($1, $2, $3) RETURNING *",
+      [groupName, groupDescription, organizerId]
+    )
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+
+// Get all groups
+const getAllGroups = function () {
+  return pool
+    .query("SELECT * FROM groups")
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.error(err);
+      return [];
+    });
+};
+
+// Get all groups a user is a member of
+const getUserGroups = function (userId) {
+  return pool
+    .query(
+      "SELECT groups.* FROM groups JOIN groupMembers ON groups.groupId = groupMembers.groupId WHERE groupMembers.userId = $1",
+      [userId]
+    )
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.error(err);
+      return [];
+    });
+};
+
+
+//Get a group by groupId:
+const getGroupById = function (groupId) {
+  return pool
+    .query("SELECT * FROM groups WHERE groupId = $1", [groupId])
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+//Update a group by groupId:
+const updateGroupById = function (groupId, groupName, groupDescription) {
+  return pool
+    .query(
+      "UPDATE groups SET groupName = $1, groupDescription = $2 WHERE groupId = $3 RETURNING *",
+      [groupName, groupDescription, groupId]
+    )
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+//Delete a group by groupId
+const deleteGroupById = function (groupId) {
+  return pool
+    .query("DELETE FROM groups WHERE groupId = $1 RETURNING *", [groupId])
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+
+
+module.exports = { createGroup, getAllGroups, getUserGroups, getGroupById, updateGroupById, deleteGroupById };
