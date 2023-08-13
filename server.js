7c6d4dd69100ec8a533ec83aef0782e4b87646ab
William <Williamtviet@hotmail.com>
2023-08-12 17:02:18 -0700

    added npm install cors due to cors issue when posting

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { getAllEvents } = require('./db/queries/event');
 const { getAllGroups } = require('./db/queries/group');
 const events = require('./db/queries/event');
 const { getAllNames } = require ('./db/queries/user')
+const cors = require('cors')
 
 
 
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(cors())
 
 
 
