3611296a886673edc18810d4cbda26661a2a493a
Warda <warda@Wardas-MacBook.local>
2023-08-16 16:36:13 -0400

    main branch merge

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const port = process.env.PORT || 3000;
 const { getAllEvents } = require('./db/queries/event');
 const { getAllGroups } = require('./db/queries/group');
 const events = require('./db/queries/event');
-const { getAllNames } = require ('./db/queries/user')
+const groups = require('./db/queries/group')
+const { getAllNames, updateProfile } = require ('./db/queries/user')
 const cors = require('cors')
+const {addAttendeeToEvent, getAttendeesCountForEvent} = require ('./db/queries/eventAttendee')
 
 
 
@@ -21,8 +23,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(cors())
-
-
 
 const userRoutes = require('./routes/userRoutes');
 const groupRoutes = require('./routes/groupRoutes');
@@ -48,6 +48,7 @@ app.get("/events", (req, res) => {
   })
 })
 
+
 app.get("/groups", (req, res) => {
   getAllGroups()
     .then((groups) => {
@@ -57,10 +58,113 @@ app.get("/groups", (req, res) => {
 });
 
 
+app.post("/createEvents", (req, res) => {
+  const { eventName, eventDescription, eventLocation, eventDate} = req.body;
+
+  if (!eventName || !eventDescription || !eventLocation || !eventDate) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  const newEvent = { // Define the newEvent variable
+    eventName,
+    eventDescription,
+    eventLocation,
+    eventDate,
+    // organizerId,
+  };
+  events.addEvent(newEvent)
+    .then(createdEvent => {
+      res.status(201).json(createdEvent);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to create event' });
+      console.error('Error creating event:', err);
+    });
+});
+
+
+app.post("/createGroups", (req, res) => {
+  const { groupName, groupDescription } = req.body;
+
+  if (!groupName || !groupDescription) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  const newGroup = {
+    groupName,
+    groupDescription,
+  };
+
+  groups.createGroup(newGroup)
+    .then(createdGroup => {
+      res.status(201).json(createdGroup); // Send the createdGroup object directly
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Failed to create group' });
+      console.error('Error creating group:', err);
+    });
+});
+
+
+
+
+
+
+app.post("/update-profile", (req, res) => {
+  const { fullName, email, phoneNumber } = req.body;
+
+  updateProfile(fullName, { email, phoneNumber })
+    .then(updatedUser => {
+      if (updatedUser) {
+        res.json({ message: "Profile updated successfully" });
+      } else {
+        res.status(500).json({ error: "Error updating profile" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Error updating profile" });
+    });
+});
+
+
+app.post('/events/:eventId/attendees', async (req, res) => {
+  const eventId = req.params.eventId;
+  const userId = req.body.userId; 
+  console.log ("TESTING")
+  console.log (eventId, userId);
+
+  try {
+    await addAttendeeToEvent(eventId, userId);
+    res.status(201).json({ message: 'Attendee added successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to add attendee to event' });
+    console.error('Error adding attendee to event:', err);
+  }
+});
+
+app.get("/events/:eventId/attendees/count", (req, res) => {
+  const eventId = req.params.eventId;
+  console.log("TESTING")
+
+  getAttendeesCountForEvent(eventId)
+    .then((attendeesCount) => {
+      res.json({ count: attendeesCount });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Failed to fetch attendees count" });
+    });
+});
+
+
+
+
+
+
+
+
 app.listen(port, (err) => {
   console.log(err || `listening on port ${port} 😎 `);
  
 });
-
 
 
